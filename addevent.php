b34e55bfe4c179686b6bd44342fb09667b4ae840
Gabe Herbert <gh@gabeherbert.com>
2023-07-10 16:10:28 -0700

    adds shortcode for addevent_links

--- addevent.php
+++ addevent.php
@@ -9,9 +9,29 @@
  * Author URI: https://yourwebsite.com
  * License: GPL2
  */
-class Addevent_Shortcode {
+
+class AddEventUtility {
+	static function get_api_key() {
+		return defined('ADDEVENT_API_KEY') ? ADDEVENT_API_KEY : '';
+	}
+
+	static function format_datetime($datetime, $is_24h = false) {
+		$format = $is_24h ? 'd-m-Y H:i' : 'm/d/Y h:i A';
+		return date($format, strtotime($datetime));
+	}
+
+	static function calculate_endtime($start, $length) {
+		// Omitted for brevity: logic for calculating end time based on length.
+	}
+
+	static function encode_parameters($parameters) {
+		return array_map('urlencode', $parameters);
+	}
+}
+class Addevent_Button {
 	public function __construct() {
 		add_shortcode( 'addevent', array( $this, 'generate_shortcode' ) );
+		add_shortcode( 'addevent_button', array( $this, 'generate_shortcode' ) );
 		add_action( 'wp_footer', array( $this, 'print_addevent_script' ) );
 	}
 
@@ -107,4 +127,61 @@ class Addevent_Shortcode {
 
 }
 
-new Addevent_Shortcode();
+class AddEventLinks {
+	public function __construct() {
+		add_shortcode('addevent_links', array($this, 'shortcode_callback'));
+		add_action('admin_notices', array($this, 'admin_notice_missing_api_key'));
+	}
+
+	public function shortcode_callback($atts = [], $content = null) {
+		$api_key = AddEventUtility::get_api_key();
+
+		if (!$api_key) {
+			if (current_user_can('manage_options')) {
+				// User is an admin and can see the notice about the missing API key.
+				return "<div class='error'><p>" . __('AddEvent Links: Please enter your API key on the <a href="options-general.php?page=addevent">AddEvent settings page</a>.') . "</p></div>";
+			} else {
+				// Fail silently for non-admin users.
+				return 'fail';
+			}
+		}
+
+		// Existing logic for generating add-to-calendar links.
+		$start = AddEventUtility::format_datetime($atts['start'], $atts['24h'] === 'true');
+		$end = isset($atts['end']) ? AddEventUtility::format_datetime($atts['end'], $atts['24h'] === 'true') :
+			AddEventUtility::calculate_endtime($atts['start'], $atts['length']);
+
+		// Prepare parameters.
+		$parameters = [
+			'client' => AddEventUtility::get_api_key(),
+			'start' => $start,
+			'end' => $end,
+			'title' => $atts['title'],
+			'description' => $atts['description'],
+			'location' => $atts['location'],
+			'timezone' => $atts['timezone']
+		];
+
+		$services = isset($atts['services']) ? explode(',', $atts['services']) : ['apple', 'google', 'office365', 'outlook', 'outlookcom', 'yahoo'];
+		$output = '';
+		foreach ($services as $service) {
+			$parameters['service'] = $service;
+			$url = 'https://www.addevent.com/dir/?' . http_build_query($parameters);
+			$output .= "<a href=\"{$url}\">{$service}</a> ";
+		}
+
+		return $output;
+
+	}
+
+	public function admin_notice_missing_api_key() {
+		$api_key = AddEventUtility::get_api_key();
+
+		if (!$api_key && current_user_can('manage_options')) {
+			echo '<div class="notice notice-error"><p>Addevent needs its client ID (API Key) defined in wp-config.php.</p></div>';
+		}
+	}
+}
+
+new AddEventLinks();
+new Addevent_Button();
