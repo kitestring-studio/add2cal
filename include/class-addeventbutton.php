f605409ade78c928430fd4289a0855f000267077
Gabe Herbert <gh@gabeherbert.com>
2023-07-11 12:09:17 -0700

    adds render code; starts adding tests

--- include/class-addeventbutton.php
+++ include/class-addeventbutton.php
@@ -15,6 +15,26 @@ class AddEventButton extends AddEventBase {
 		wp_enqueue_script('addevent', 'https://cdn.addevent.com/libs/atc/1.6.1/atc.min.js', array(), null, true);
 
 		// Proceed with the button rendering...
+		// Prepare data for rendering.
+		$parameters['start'] = self::format_date($parameters['start'], $parameters['24h']);
+		$parameters['end'] = self::calculate_end_date($parameters['start'], $parameters['end'], $parameters['length'], $parameters['24h']);
+
+		// Start output buffering.
+		ob_start();
+		?>
+		<div title="<?php echo esc_attr($parameters['title']); ?>" class="<?php echo esc_attr($parameters['class']); ?>">
+			Add to Calendar
+			<span class="start"><?php echo esc_html($parameters['start']); ?></span>
+			<span class="end"><?php echo esc_html($parameters['end']); ?></span>
+			<span class="timezone"><?php echo esc_html($parameters['timezone']); ?></span>
+			<span class="title"><?php echo esc_html($parameters['event_title']); ?></span>
+			<span class="description"><?php echo esc_html($parameters['description']); ?></span>
+			<span class="location"><?php echo esc_html($parameters['location']); ?></span>
+		</div>
+		<?php
+
+		// End output buffering and return buffered output.
+		return ob_get_clean();
 	}
 
 	// Other necessary methods like button rendering, etc...
--- include/class-addeventlinks.php
+++ include/class-addeventlinks.php
@@ -18,6 +18,37 @@ class AddEventLinks extends AddEventBase {
 		}
 
 		// Proceed with the links rendering...
+		$parameters['start'] = self::format_date($parameters['start'], $parameters['24h']);
+		$parameters['end'] = self::calculate_end_date($parameters['start'], $parameters['end'], $parameters['length'], $parameters['24h']);
+
+		// Prepare the data for the URL.
+		$url_data = array(
+			'client' => ADDEVENT_API_KEY,
+			'start' => rawurlencode($parameters['start']),
+			'end' => rawurlencode($parameters['end']),
+			'title' => rawurlencode($parameters['event_title']),
+			'description' => rawurlencode($parameters['description']),
+			'location' => rawurlencode($parameters['location']),
+			'timezone' => rawurlencode($parameters['timezone']),
+		);
+
+		// Start output buffering.
+		ob_start();
+		foreach (self::get_services() as $service) {
+			// Only include the services specified by the 'services' attribute.
+			if (!in_array($service, $parameters['services'])) {
+				continue;
+			}
+
+			$url_data['service'] = $service;
+			$url = 'https://www.addevent.com/dir/?' . http_build_query($url_data);
+			?>
+            <a href="<?php echo esc_url($url); ?>"><?php echo esc_html(ucfirst($service)); ?></a>
+			<?php
+		}
+
+		// End output buffering and return buffered output.
+		return ob_get_clean();
 	}
 
 	public static function admin_notice_missing_api_key() {
