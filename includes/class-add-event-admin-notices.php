e5ac91b08048e01b84a383775acd18011c6cfaf7
Gabe Herbert <gh@gabeherbert.com>
2023-07-11 14:18:39 -0700

    makes addevent admin notice non-dismissable

--- includes/class-add-event-admin-notices.php
+++ includes/class-add-event-admin-notices.php
@@ -10,7 +10,7 @@ class Add_Event_Admin_Notices {
 	public static function display_admin_notices() {
 		if ( ! defined( 'ADDEVENT_API_KEY' ) ) {
 			?>
-            <div class="notice notice-warning is-dismissible">
+            <div class="notice notice-warning">
                 <p><?php echo __( 'The AddEvent plugin requires the AddEvent API key to be defined in your wp-config.php file.', 'addevent' ); ?></p>
             </div>
 			<?php
