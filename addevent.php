6d247464deba8b932e8925c1b828004f70ce47ca
Gabe Herbert <gh@gabeherbert.com>
2023-07-27 14:29:47 +0100

    updates plugin name; updates plugin URI

--- addevent.php
+++ addevent.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: AddEvent Shortcodes
- * Plugin URI: https://github.com/user/plugin
+ * Plugin Name: Add2Cal Shortcodes
+ * Plugin URI: https://github.com/kitestring-studio/add2cal
  * Description: Provides two shortcodes, 'addevent_button' and 'addevent_links', for adding events to various calendar services.
  * Version: 1.0.0
  * Author: Kitestring Studio
